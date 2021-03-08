110210464d226dfa0a62912e71ac22127bccb36b
Dex <59426458+Dixboi@users.noreply.github.com>
2021-03-08 11:18:47 +0800

    Create Stack_cla.py

---
+++ Data Structures/Stack/Stack_cla.py
@@ -0,0 +1,25 @@
+class Stack:
+    def __init__(self):
+        self.item = []
+
+    def is_empty(self):
+        return self.item == []
+
+    def push(self, item):
+        self.item.insert(0, item)
+
+    def pop(self):
+        return self.item.pop(0)
+
+    def print_stack(self):
+        print(self.item)
+
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(2)
+    s.push(3)
+    s.push(4)
+    s.push(5)
+    s.pop()
+    s.print_stack()
