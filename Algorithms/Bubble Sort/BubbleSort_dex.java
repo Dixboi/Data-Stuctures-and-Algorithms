d231f9ebed924190432d033dddefe967c13cc3d1
Dex <59426458+Dixboi@users.noreply.github.com>
2021-03-08 11:25:50 +0800

    Create BubbleSort_dex.java

---
+++ Algorithms/Bubble Sort/BubbleSort_dex.java
@@ -0,0 +1,23 @@
+public class Main
+{
+	
+    static void bubbleSort(int[] listToBeSorted) {
+        for (int number1 = 0; number1 < listToBeSorted.length; number1++) {
+            for (int number2 = 0; number2 < listToBeSorted.length; number2++) {
+                if (listToBeSorted[number1] < listToBeSorted[number2]) {
+                    int temporaryValue = listToBeSorted[number1];
+                    listToBeSorted[number1] = listToBeSorted[number2];
+                    listToBeSorted[number2] = temporaryValue;
+                }
+            }
+        }
+        
+        for (int number = 0; number < listToBeSorted.length; number++) {
+            System.out.print(listToBeSorted[number] + ", ");
+        }
+    }
+    
+	public static void main(String[] args) {
+    //Insert main code
+	}
+}
