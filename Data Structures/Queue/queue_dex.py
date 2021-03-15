52b158c475bb29df89c520d888b73467144f6e25
Dex <59426458+Dixboi@users.noreply.github.com>
2021-03-15 10:51:59 +0800

    Create queue_dex.py
    Queue data structure

---
+++ Data Structures/Queue/queue_dex.py
@@ -0,0 +1,48 @@
+
+class Queue:
+    
+    def __init__(self, size = 5):
+        self.size = size # size of the queue
+        self.queue = []
+    
+    # Returns if the queue is full
+    def isFull(self):
+        return len(self.queue) == self.size
+    
+    # Returns if the queue is empty
+    def isEmpty(self):
+        return len(self.queue) == 0
+    
+    # Returns the number of items of the queue
+    def count(self):
+        if self.isEmpty(): return 0
+        if self.isFull(): return self.size
+        return len(self.queue)
+    
+    # Adds an element on the first index
+    def enqueue(self, item):
+        if self.isFull() == False:
+            self.queue.insert(0, item)
+    
+    # Removes the first inserted element
+    def dequeue(self):
+        if self.isEmpty() == False:
+            self.queue.pop()
+    
+    # Return the first inserted element        
+    def peek(self):
+        if self.isEmpty(): return "Empty queue"
+        return self.queue[-1]
+    
+    # Displays the details of the queue
+    def details(self):
+        print("Empty:", self.isEmpty())
+        print("Full:", self.isFull())
+        print("Current count:", self.count())
+        print("Queue:")
+        
+        for i in self.queue:
+            print(i, end = " ")
+            
+        print()
+		
