11e4430853baebd2bf322c76d4f43d8c4faf130a
Dex <59426458+Dixboi@users.noreply.github.com>
2021-04-05 11:48:09 +0800

    Remove indexOf, removeDataOn, getDataOn, length

--- Data Structures/Linked List/SingleLinkedList.py
+++ Data Structures/Linked List/SingleLinkedList.py
@@ -20,63 +20,6 @@ class LinkedList():
             
         current_node.next_node = new_node
         
-	# Returning the length of the list
-    def length(self):
-        count = 0 
-        current_node = self.head
-        
-        while current_node.next_node != None:
-            current_node = current_node.next_node
-            count += 1
-            
-        return count
-        
-	# Getting the data on a specific index
-    def getDataOn(self, index):
-        if index >= self.length() or index < 0:
-            return "None"
-        
-        current_index = 0
-        current_node = self.head
-        
-        while True:
-            current_node = current_node.next_node
-            if current_index == index:
-                return current_node.data
-                
-            current_index += 1
-            
-	# Removing data on a specific index
-    def removeDataOn(self, index):
-        if index >= self.length() or index < 0:
-            return None
-            
-        current_index = 0
-        current_node = self.head
-        
-        while True:
-            prev_node = current_node
-            current_node = current_node.next_node
-            if current_index == index:
-                prev_node.next_node = current_node.next_node
-                break
-                
-            current_index += 1
-        
-	# Finding the index of a specific data
-    def indexOf(self, data):
-        current_index = 0
-        current_node = self.head
-
-        while current_node.next_node != None:
-            current_node = current_node.next_node
-            if current_node.data == data:
-                return current_index
-                
-            current_index += 1
-            
-        return None
-        
 	# Displaying all elements of the list
     def display(self):
         current_node = self.head
